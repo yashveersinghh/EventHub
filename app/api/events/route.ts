bbba03c9db6b0855742a06acdad1368e02a6f5a0
Yashveer singh <yashveersingh1921@gmail.com>
2026-06-16 22:07:15 +0530

    added fetch API routes

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import Event from "@/database/event.model";
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import{ v2 as cloudinary } from 'cloudinary';
 
 export async function POST (req: NextRequest) {
     try {
@@ -16,11 +17,41 @@ export async function POST (req: NextRequest) {
             return NextResponse.json({ message: 'Invalid JSON data format', error: e}, { status: 400 })
         }
 
+        const file = formData.get('image') as File;
+
+        if(!file){
+            return NextResponse.json({ message: 'Image file is required' }, { status: 400 });
+        }
+
+        const arrayBuffer = await file.arrayBuffer(); //blob
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'event-next' }, (error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+            }).end(buffer);
+        })
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
+
         const createdEvent = await Event.create(event);
 
         return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 });
     } catch (e) {
         console.error('Error creating event:', e);
         return NextResponse.json({ message: 'Error creating event', error: e instanceof Error ? e.message : 'Unknown error'}, { status: 500 });
+    }
+}
+
+export async function GET () {
+    try {
+        await connectDB();
+
+        const events = await Event.find().sort({ createdAt: -1 });
+
+        return NextResponse.json({ message: 'Events fetched successfully', events }, { status: 200 });
+    } catch (e) {
+        return NextResponse.json({ message: 'Error fetching events', error: e }, { status: 500 });
     }
 }
