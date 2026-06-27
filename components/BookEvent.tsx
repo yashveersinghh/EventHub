d4bd38f51c127fab042e4fd208105482d6c1b3b0
Yashveer singh <yashveersingh1921@gmail.com>
2026-06-27 20:27:48 +0530

    completed the book event component

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,6 +1,39 @@
+'use client';
+import { useState } from "react"
+
 const BookEvent = () => {
+  const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 1000);
+  }
   return (
-    <div>BookEvent</div>
+    <div id="book-event">
+      {submitted ? (
+        <p className="text-sm">Thank you for signing up!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email Address</label>
+            <input 
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              placeholder="Enter your email address"
+             />
+          </div>
+
+          <button type="submit" className="button-submit">Sign Up</button>
+        </form>
+      )
+    }
+    </div>
   )
 }
 
